09f941d5049432df904bae08704e52280f0f45b4
yjen <yjen@berkeley.edu>
2016-04-23 01:55:22 -0700

    simplified

--- line_cut.py
+++ line_cut.py
@@ -3,20 +3,17 @@ from robot import *
 from geometry_msgs.msg import Pose
 import numpy as np
 import PyKDL
-from scipy.interpolate import UnivariateSpline
 import multiprocessing
-import Tkinter
 import tfx
-import IPython
 import fitplane
 
 
 def initialize():
     global psm1_position, psm2_position
-    psm2_initial = [-0.0912484814441, 0.0674414679811, -0.0736081506167, 0.888720223413, -0.254590620512, -0.302364852004, -0.232240127275]
+    psm2_initial = [-0.0912484814441, 0.0794414679811, -0.0736081506167, 0.888720223413, -0.254590620512, -0.302364852004, -0.232240127275]
     psm1_initial = [0.0298111217581, 0.0255537141169, -0.111452040502, 0.617571885272, 0.59489495214, 0.472153066551, 0.204392867261]
     start_frame1 = get_frame(psm1_initial)
-    start_frame2 = get_frame(psm2_initial)
+    start_frame2 = get_frame2(psm2_initial)
     psm1.move_cartesian_frame(start_frame1)
     psm2.move_cartesian_frame(start_frame2)
     psm1.open_gripper(80)
@@ -32,32 +29,47 @@ def calibrate():
     return plane
 
 def get_frame(pos):
-    pt = fitplane.project_onto_plane(pos[0:3])
+    pt = fitplane.project_onto_plane(plane, pos[0:3])
     return tfx.pose(pt.tolist(), pos[3:7])
+
+
+def get_frame2(pos):
+    return tfx.pose(pos[0:3], pos[3:7])
 
 def cut():
     psm1.open_gripper(-30)
     time.sleep(2.5)
-    pos = psm1_position
-    pos[0] = pos[0] - 0.001
-    psm1.move_cartesian_frame(get_frame(pos))
     psm1.open_gripper(80)
     time.sleep(2.35)
 
 def move_to_next():
     pos = psm1_position
-    pos[0] = pos[0] + 0.01
-    pos[2] = pos[2] - 0.0004
+    pos[0] = pos[0] + 0.0045
+    pos[1] = pos[1] + 0.0008
     psm1.move_cartesian_frame(get_frame(pos))
+
+def psm1_translation(translation):
+    pos = psm1_position
+    pos[0] += translation[0]
+    pos[1] += translation[1]
+    pos[2] += translation[2]
+    psm1.move_cartesian_frame(get_frame(pos))
+
+def psm2_translation(translation):
+    pos = psm2_position
+    pos[0] += translation[0]
+    pos[1] += translation[1]
+    pos[2] += translation[2]
+    psm1.move_cartesian_frame(get_frame2(pos))
 
 def grab_gauze():
     pos = psm2_position
     pos[2] = pos[2] - 0.013
-    psm2.move_cartesian_frame(get_frame(pos))
+    psm2.move_cartesian_frame(get_frame2(pos))
     psm2.open_gripper(-30)
     time.sleep(2.5)
     pos[2] = pos[2] + 0.01
-    psm2.move_cartesian_frame(get_frame(pos))
+    psm2.move_cartesian_frame(get_frame2(pos))
 
 if __name__ == '__main__':
     plane = calibrate()
@@ -70,9 +82,9 @@ if __name__ == '__main__':
 
     initialize()
 
-    grab_gauze()
+    # grab_gauze()
 
-    for i in range(25):
+    for i in range(30):
         cut()
         move_to_next()
 
